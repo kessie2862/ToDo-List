057d315812957f7e6846c7852f03b16ed610eb0f
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-05-11 17:43:32 +0300

    Add save to local storage

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,10 @@
-import 'jsdom-global/register.js';
-import 'jest-localstorage-mock';
+import "jsdom-global/register.js";
+import "jest-localstorage-mock";
 
 // importing functions I want to test
-import './index.css';
+import "./index.css";
 
-const { tasks, addNewTask, deleteTask } = require('./index.js');
+const { tasks, addNewTask, deleteTask } = require("./index.js");
 
 const localStorageMock = {
   getItem: jest.fn(),
@@ -14,33 +14,33 @@ const localStorageMock = {
 
 global.localStorage = localStorageMock;
 
-describe('Task list app', () => {
+describe("Task list app", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  describe('addNewTask', () => {
-    it('adds a new task to the list', () => {
+  describe("addNewTask", () => {
+    it("adds a new task to the list", () => {
       const initialTasksLength = tasks.length;
-      const taskDescription = 'Test task description';
+      const taskDescription = "Test task description";
       addNewTask(taskDescription);
       expect(tasks.length).toBe(initialTasksLength + 1);
       expect(tasks[initialTasksLength].description).toBe(taskDescription);
     });
 
-    // it('saves the new task to localStorage', () => {
-    //   const taskDescription = 'Test task description';
-    //   addNewTask(taskDescription);
-    //   expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    //   expect(localStorage.setItem).toHaveBeenCalledWith(
-    //     'tasks',
-    //     JSON.stringify(tasks),
-    //   );
-    // });
+    it("saves the new task to localStorage", () => {
+      const taskDescription = "Test task description";
+      addNewTask(taskDescription);
+      expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+      expect(localStorage.setItem).toHaveBeenCalledWith(
+        "tasks",
+        JSON.stringify(tasks)
+      );
+    });
   });
 
-  describe('deleteTask', () => {
-    it('deletes the task from the list', () => {
+  describe("deleteTask", () => {
+    it("deletes the task from the list", () => {
       const initialTasksLength = tasks.length;
       const taskIndex = 0;
       deleteTask(taskIndex);
@@ -48,7 +48,7 @@ describe('Task list app', () => {
       expect(tasks.every((task, index) => task.index === index)).toBe(true);
     });
 
-    it('saves the updated task list to localStorage', () => {
+    it("saves the updated task list to localStorage", () => {
       const taskIndex = 0;
       const initialTasksLength = tasks.length;
       deleteTask(taskIndex);
@@ -56,7 +56,7 @@ describe('Task list app', () => {
       expect(tasks.every((task, index) => task.index === index)).toBe(true);
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
       expect(localStorage.setItem).toHaveBeenCalledWith(
-        'tasks',
+        "tasks",
         JSON.stringify(tasks)
       );
     });
