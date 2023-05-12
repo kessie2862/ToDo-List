fcfcc1319a76040e3cedb0db30780b54d27a4272
kessie <kayp3477@gmail.com>
2023-05-12 14:19:27 +0000

    Removed unwanted comments

--- src/status.js
+++ src/status.js
@@ -1,13 +1,9 @@
-// import { saveTasks } from './index.js';
-
 const markAsCompleted = (task) => {
   task.completed = true;
-  // saveTasks();
 };
 
 const markAsIncomplete = (task) => {
   task.completed = false;
-  // saveTasks();
 };
 
 export { markAsCompleted, markAsIncomplete };
